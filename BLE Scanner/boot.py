cf2b5e5a31bb103dc40f9fc90269b8bbe80865e4
Enrico Gamil Toros de Chadarevian <enrico.dechadarevian@student.htw-berlin.de>
2024-02-08 23:13:50 +0100

    Removed libs as they are now downloaded on boot.

--- BLE Scanner/boot.py
+++ BLE Scanner/boot.py
@@ -1,13 +1,24 @@
 from util import wifiManager
 from util import utils
-from util import mqttClient
 import config
+import mip
 
 utils.df()
 if (config.NET):
     wifiManager.connect()
+if (wifiManager.isConnected()):
+    # check if aioble is installed
+    try:
+        import aioble
+    except ImportError:
+        mip.install("aioble")
+    try:
+        import primitives
+    except ImportError:
+        mip.install("github:peterhinch/micropython-async/v3/primitives")
 try:
     if (config.MQTT_START):
+        from util import mqttClient
         mqttClient.MQTTConnect()
 except OSError:
     mqttClient.errorFallback()
