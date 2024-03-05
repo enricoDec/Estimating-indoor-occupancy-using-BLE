2a888210584f7f3ae6d8b241df95c218792a562c
Enrico Gamil Toros de Chadarevian <enrico.dechadarevian@student.htw-berlin.de>
2024-03-05 22:08:37 +0100

    adds handling of partioned data by interface

--- Smartphone Counter/countSmartphones.py
+++ Smartphone Counter/countSmartphones.py
@@ -16,6 +16,7 @@ def appendUnknownDeviceIfNotAlreadyIn(unknownDevices, descriptor):
     if (alreadyIn == False):
         unknownDevices.append(descriptor)
 
+
 inputFile = sys.argv[1]
 if (inputFile == None):
     print("No input file specified")
@@ -33,7 +34,7 @@ with open("unknownDevices.json", "r") as file:
 counter = 0
 for result in json_data["scanresult"]:
     descriptor = result["descriptor"]
-    if (rule_based_classification(descriptor, knownSmartphones)):
+    if (descriptor is not None and rule_based_classification(descriptor, knownSmartphones)):
         counter += 1
     else :
         appendUnknownDeviceIfNotAlreadyIn(unknownDevices, descriptor)
@@ -41,5 +42,8 @@ for result in json_data["scanresult"]:
 with open("unknownDevices.json", "w") as file:
     json.dump(unknownDevices, file)
 
-result = {"smartphones": counter, "room": json_data["room"]}
-print(json.dumps(result))
+# append smartphones to the json_data
+json_data["smartphones"] = counter
+
+
+print(json.dumps(json_data))
